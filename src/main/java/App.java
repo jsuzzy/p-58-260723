d65062325e1176369fe40f12412bf71a77f9aba9
jeongsuji <sujijeong11@gmail.com>
2026-07-23 11:16:44 +0900

    AppTest t5- 목록

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
     private int lastId = 0;
+    List<com.back.WiseSaying> wiseSayings = new ArrayList<>();
+
     private String cmd;
 
     public App(Scanner sc){
@@ -24,7 +28,22 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
                     ++lastId;
+
+                    com.back.WiseSaying wiseSaying = new com.back.WiseSaying(lastId, saying, author);
+                    wiseSayings.add(wiseSaying);
+
                     System.out.printf("%d번 명언이 등록되었습니다.",lastId);
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
+                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
+
                 }
                 case "종료" -> {
                     return;
